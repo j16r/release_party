62bb722a579aec510876cb8ffedfa4948cb0be95
John Barker <jebarker@gmail.com>
2011-05-19 23:20:11 +0800

    argument_required handling for better feedback, load different engines

--- lib/release_party/release_party.rb
+++ lib/release_party/release_party.rb
@@ -1,7 +1,6 @@
 require 'capistrano'
 require 'pivotal-tracker'
 require 'mail'
-require 'haml'
 
 require File.join(File.dirname(__FILE__), 'environment')
 
@@ -26,20 +25,36 @@ module Capistrano::ReleaseParty
         task 'finished' do
           raise ArgumentError, "Release finished without being started" if @env.nil?
 
-          # Record when the release finished
-          @env.release_finished = Time.now
+          begin
 
-          load_tracker_progress @env
-          load_git_progress @env
+            # Record when the release finished
+            @env.release_finished = Time.now
 
-          if @env.send_email
-            body = load_email_content @env
-            deliver_notification(@env, body)
+            load_git_progress @env
+
+            load_tracker_progress @env
+
+            # Do pivotal story delivery
+            if @env.deliver_stories
+              commit_tracker_progress @env
+            end
+
+            # Send an email notification
+            if @env.send_email
+              body = load_email_content @env
+              deliver_notification(@env, body)
+            end
+
+          #rescue ArgumentError => error
+          #  puts "ERROR: #{error.message}"
           end
-
         end
       end
     end
+  end
+
+  def commit_tracker_progress(env)
+
   end
 
   def load_git_progress(env)
@@ -47,39 +62,53 @@ module Capistrano::ReleaseParty
   end
 
   def load_email_content(env)
-    # Mail a release party notice to someone?
+    arguments_required env, :template_engine, :template
+
     template = env.template
+    raise ArgumentError, "Missing template file #{template}" unless File.exists?(template)
+
     engine = \
-      if template && FileTest.exists?(template)
+      case env.template_engine
+      when :haml
+        load_haml
         Haml::Engine.new(File.read(template))
+
+      when :erb
+        load_erb
+        ERB.new(File.read(template))
+
+      else
+        raise ArgumentError, "Unsupported template engine #{env.template_engine}"
       end
 
-    if engine
-      engine.render env
-    else
-      'Hello'
-    end
+    engine.render env
   end
 
   def load_tracker_progress(env)
-    # For pivotal info
-    if env.project_id? && env.project_api_key?
-      PivotalTracker::Client.token = env.project_api_key
+    arguments_required env, :project_id, :project_api_key
 
-      begin
-        project = PivotalTracker::Project.find(env.project_id)
-      rescue RestClient::Request::Unauthorized => e
-        puts "Unable to authenticate with pivotal, perhaps your API key is wrong. Message: #{e.message}"
-      end
+    PivotalTracker::Client.token = env.project_api_key
 
-      env.instance_eval do
-        self.finished_stories = project.stories.all(:state => 'finished')
-        self.known_bugs = project.stories.all(:state => ['unstarted', 'started'], :story_type => 'bug')
-      end
+    begin
+      env.project = PivotalTracker::Project.find(env.project_id)
+    rescue RestClient::Request::Unauthorized => e
+      puts 'Unable to authenticate with pivotal, perhaps your API key is wrong.' +
+        "Message: #{e.message}"
+    end
+
+    env.instance_eval do
+      self.finished_stories = \
+        project.stories.all(:state => 'finished')
+      self.known_bugs = \
+        project.stories.all(:state => ['unstarted', 'started'], :story_type => 'bug')
     end
   end
 
   def deliver_notification(env, body)
+    arguments_required env,
+      :smtp_address, :smtp_port, :from_address,
+      :email_notification_to, :subject
+
     Mail.defaults do
       delivery_method :smtp,
         :address => env.smtp_address,
@@ -94,6 +123,31 @@ module Capistrano::ReleaseParty
         body body
       end
     end
+  end
+
+  def arguments_required(env, *arguments)
+    missing = arguments.reject do |argument|
+      env.send("#{argument}?".to_sym)
+    end
+    unless missing.empty?
+      raise ArgumentError, "Parameters #{missing.join(', ')} must be defined"
+    end
+  end
+
+  def load_haml
+    require 'haml'
+  rescue LoadError => error
+    raise ArgumentError,
+      'Unable to load HAML, you need to make sure the haml gem is installed ' 
+      'if it is specified as the template engine'
+  end
+
+  def load_erb
+    require 'erb'
+  rescue LoadError => error
+    raise ArgumentError,
+      'Unable to load ERB, you need to make sure the erb gem is installed ' 
+      'if it is specified as the template engine'
   end
 
 end
