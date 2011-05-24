3eba91c3d532beba27762aa1b833bd219288bac4
John Barker <jebarker@gmail.com>
2011-05-24 18:03:33 +0800

    Lazy load capistrano configuration variables, loading them can cause problems as some may be blocks that have side effects

--- lib/release_party/environment.rb
+++ lib/release_party/environment.rb
@@ -15,13 +15,11 @@ class Environment
       @variables[$1.to_sym] = args.first || block
 
     when /\A(.*)\?\Z/
-      @variables.key?($1.to_sym)
+      @variables[$1.to_sym]
 
     else
-      unless @variables.key?(method_id)
-        raise ArgumentError, "No value for #{method_id} defined"
-      end
-      value = @variables[method_id.to_sym]
+      key = method_id.to_sym
+      value = @variables[key] || cap_config.fetch(key, defaults[key])
       return value.call if value.is_a?(Proc)
       value
 
@@ -34,27 +32,23 @@ class Environment
     @variables = @variables.merge release_file.variables
   end
 
-  def load_capistrano_defaults(cap_config)
-    cap_config.variables.keys.each do |key|
-      self.send("#{key}=", cap_config.fetch(key, @variables[key.to_sym]))
-    end
-  end
-
-  def load_defaults
-    self.user = `git config user.name`.chomp
-    self.branch = 'master'
-    self.stage = 'staging'
-    self.domain = 'http://releaseparty.org'
-    self.display_name = 'Release Party'
-    self.from_address = 'Release Party <releaseparty@noreply.org>'
-    self.smtp_address = 'localhost'
-    self.smtp_port = 25
-    self.subject = Proc.new {"A release of #{display_name} was released to #{domain}"}
-    self.finished_stories = []
-    self.known_bugs = []
-    self.send_email = true
-    self.template_engine = :haml
-    self.deliver_stories = false
+  def defaults
+    {
+      :user => `git config user.name`.chomp,
+      :branch => 'master',
+      :stage => 'staging',
+      :domain => 'http://releaseparty.org',
+      :display_name => 'Release Party',
+      :from_address => 'Release Party <releaseparty@noreply.org>',
+      :smtp_address => 'localhost',
+      :smtp_port => 25,
+      :subject => Proc.new {"A release of #{display_name} was released to #{domain}"},
+      :finished_stories => [],
+      :known_bugs => [],
+      :send_email => true,
+      :template_engine => :haml,
+      :deliver_stories => false,
+    }
   end
 
 end
